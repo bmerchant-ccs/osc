ee0a2960467470925c542c5f44e5f919a905965a
Brad Merchant <bmerchant@campbell.local>
2024-10-28 10:14:13 -0400

    Complete rewrite

--- nvue-patch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ansible-galaxy collection install nvidia.nvue
-patch -u -b ~/.ansible/collections/ansible_collections/nvidia/nvue/roles/mlag/tasks/mlag-config.yml -i mlag-config.patch
